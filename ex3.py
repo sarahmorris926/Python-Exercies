3324cc225fc2007f9edcaa0d17175c9954dc642f
Sarah Morris <sarah.morris9@gmail.com>
2018-03-30 23:30:15 -0500

    Finished 5 exercises

---
+++ ex3.py
@@ -0,0 +1,23 @@
+# this will print the string
+print("I will now count my chickens:")
+
+# this will find the sum of the equations and output them to the console
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 -  25 * 3 % 4)
+
+# this will print the string
+print("Now I will count the eggs:")
+
+# this will print the sum of the equations and output them to the console
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# this will print the string
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# this will print the sum of the equations and output them to the console
+print(3 + 2 < 5 - 7)
+
+# this will print the sum of the equations and output them to the console
+print("is it greater?", 5 > -2)
+print("is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
---
+++ ex5.py
@@ -0,0 +1,9 @@
+my_name = "Sarah"
+my_age = 25
+my_height = 165 #inches
+my_eyes = 'Blue'
+my_hair = "Brown"
+
+print(f"Let's talk about {my_name}")
+print(f"She's {my_height} inches tall, and {my_age} years old.")
+print(f"She's got {my_eyes} eyes and {my_hair} hair.")
